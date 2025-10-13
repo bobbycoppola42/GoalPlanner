2f16be10125b226067f773d098e85d716284dee4
Bobby Coppola <bobbycoppola42@gmail.com>
2025-10-13 16:26:01 -0400

    Implemented Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import GoalPlanner from './GoalPlanner.jsx';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
